f08ff5853e1763c93084742d80dbe1ef024ac493
Tom <josephdotson@belkonar.com>
2025-01-11 19:56:19 -0500

    wo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,15 @@
 #include "mainwindow.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
 #include <QtGui/qicon.h>
+#include <iostream>
+
+const char *sql = "CREATE TABLE IF NOT EXISTS COMPANY("
+                  "ID INT PRIMARY KEY     NOT NULL,"
+                  "NAME           TEXT    NOT NULL,"
+                  "AGE            INT     NOT NULL,"
+                  "ADDRESS        CHAR(50),"
+                  "SALARY         REAL );";
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle(tr("Finance!"));
@@ -9,6 +19,22 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     newAct->setIcon(QIcon::fromTheme(QIcon::ThemeIcon::DocumentNew));
 
     fileMenu->addAction(newAct);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("/Users/adf/finance.db");
+
+    bool ok = db.open();
+
+    if (!ok) {
+        std::cerr << "what";
+    } else {
+        QSqlQuery query;
+        if (!query.exec(sql)) {
+            std::cerr << "err: " << ok;
+        } else {
+            std::cerr << "good to go";
+        }
+    }
 }
 
 MainWindow::~MainWindow() {}
