e4419c1a178e1c63f93841a2f0bba792cdd8922c
Tom <josephdotson@belkonar.com>
2025-01-11 20:39:24 -0500

    writable

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,12 @@
 #include "mainwindow.h"
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QStandardPaths>
+#include <QString>
+#include <QtCore/qhashfunctions.h>
+#include <QtCore/qstandardpaths.h>
 #include <QtGui/qicon.h>
+#include <QtWidgets/qapplication.h>
 #include <iostream>
 #include <sstream>
 
@@ -13,10 +18,6 @@ const char *sql = "CREATE TABLE IF NOT EXISTS COMPANY("
                   "SALARY         REAL );";
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-    std::ostringstream connStrStream;
-    connStrStream << getenv("HOME") << "/finance.db";
-    std::string connStr = connStrStream.str();
-
     setWindowTitle(tr("Finance!"));
     fileMenu = menuBar()->addMenu(tr("&File"));
 
@@ -26,7 +27,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     fileMenu->addAction(newAct);
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(connStr.c_str());
+    QString appData =
+        QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+
+    // TODO: At some point, make this user selectable;
+    auto dbFile = appData + "/finance.db";
+
+    qInfo() << "Db:" << dbFile;
+
+    db.setDatabaseName(dbFile);
 
     bool ok = db.open();
 
